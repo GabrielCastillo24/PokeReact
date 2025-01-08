3f3cc20034c91f0e8370159db75fafe1e237868e
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2025-01-08 11:06:14 +0100

    commit 08/01/2025

--- pokereact/src/componets/Pokelista.js
+++ pokereact/src/componets/Pokelista.js
@@ -1,18 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {Link , useNavigate} from "react-router-dom"
 const Pokelista = (props) =>{
-
     const[pokemons, setPokemons]= useState([]);
+    const navigate = useNavigate();
     useEffect(() =>{
         axios.get("https://pokeapi.co/api/v2/pokemon").then(response =>{
             setPokemons(response.data.results);
         })
     },[]);
-
+    const funcionNavegar= (p) =>{
+        navigate(`/pokemon/${p}`)
+    }
  
     return <div>
             <h1>Lista :</h1>
-            {pokemons.map(p => <p>Bo nadal</p>)}
+            {pokemons.map(p => {
+                return<>
+                <p>Este pokemon es {p.name}</p>
+                <div onClick={()=>{navigate("/pokemon/"+p.name)}}>navegar</div>
+                <Link to={"/pokemon/"+p.name}>Navegar</Link>
+                </>
+}
+)}
     </div>
 }
 
